5e0b9f010334f16ad26b0894217d694df4997ea0
art-idea5642 <chertezhy.ru@gmail.com>
2024-10-06 14:20:12 +0400

    Initial commit

--- src/main/java/com/example/shoppingcart/ShoppingcartApplication.java
+++ src/main/java/com/example/shoppingcart/ShoppingcartApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppingcartApplication {
+public class ShoppingcartApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingcartApplication.class, args);
